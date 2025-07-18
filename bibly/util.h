f753831438659ceb748dfeb8f18d406fd4616615
rald <thecoconutnutisagiantnut@gmail.com>
2025-07-19 07:44:52 +0800

    OK

--- bibly/util.h
+++ bibly/util.h
@@ -21,11 +21,11 @@ double drand() {
 }
 
 char *trim(char *a) {
-    char *p = a, *q = a;
-    while (isspace(*q))            ++q;
-    while (*q)                     *p++ = *q++;
-    *p = '\0';
-    while (p > a && isspace(*--p)) *p = '\0';
+  char *p = a, *q = a;
+  while (isspace(*q)) ++q;
+  while (*q) *p++ = *q++;
+  *p = '\0';
+  while (p > a && isspace(*--p)) *p = '\0';
 	return a;
 }
 
